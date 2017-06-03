4759740a04204a7889d8eedea35502dcd37acab6
Fulva幽 <aurorasign@tp.edu.tw>
2017-06-03 13:27:36 +0800

    加了一些全域變數與函數

--- RedGreenLight/RedGreenLight.ino
+++ RedGreenLight/RedGreenLight.ino
@@ -1,7 +1,15 @@
 /*
 硂琌厚縊祘Α
  */
-
+int green = 0; //北厚縊獹办跑计
+int red = 0; //北縊獹办跑计
+int orange = 0;//北独縊獹办跑计
+int mode = 0;//癘拘家Α0箇砞家Α1眏家Α2代み瞯家Α
+int time = 0;//计
+int GrTime = 0;//厚縊砞﹚丁
+int RdTime = 0;//縊砞﹚丁
+void displayTime(int time);//陪ボ丁ㄧ计
+void settingMode();//㊣秈砞﹚厚縊縊丁家Α
 
 // the setup function runs once when you press reset or power the board
 void setup() {
