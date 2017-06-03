2edcf32e4a9576bfd28e19db4359c40726216b6a
Fulva幽 <aurorasign@tp.edu.tw>
2017-06-03 23:19:26 +0800

    寫好DefaultMode了!!!

--- RedGreenLight/RedGreenLight.ino
+++ RedGreenLight/RedGreenLight.ino
@@ -8,14 +8,16 @@ void displayTime(int time);//顯示時間的函數
 void settingMode();//呼叫進入設定綠燈紅燈時間的模式
 void DefaultMode();//進入預設模式
 void ForceMode();//進入強制模式
+void HeartRateMode();//進入測心率模式
+boolean changeMode();//轉換模式測試
 
 int Button1();//回傳個位數的按鈕是否按下，是則回傳1，否則回傳0
 int Button2();//回傳十位數的按鈕是否...
 int ButtonS();//回傳設定紐...
 
-int Button1=LOW;//紀錄上一次檢查button1狀態的變數
-int Button2=LOW;//紀錄上一次...
-int Button3 = LOW;//紀錄...
+int Buttons1 = 0;//紀錄上一次檢查button1狀態的變數
+int Buttons2 = 0;//紀錄上一次...
+int ButtonsS = 0;//紀錄...
 int pause = 0;//暫停的切換器
 
 void Green(int val);//綠燈亮，val 傳入LOW關閉，HIGH打開
@@ -30,11 +32,16 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-
+	switch (mode) {
+	case 0:
+		DefaultMode();
+	case 1:
+		ForceMode();
+	case 2:
+		HeartRateMode();
 	}
-
-
 }
+
 
 void displayTime(int time)
 {
@@ -48,15 +55,38 @@ void settingMode()
 
 void DefaultMode()
 {
-	int light = 0;//0 is green, 1 is orange, 2 is red
-	int time = GrTime*10;//綠燈時間的百毫秒
-	if (pause == 0) {
-		if (time == 0) {
-			light = light + 1;
-			if (light == 3)
-				light = 0;
-			
+	do {
+		int light = 0;//0 is green, 1 is orange, 2 is red
+		int time = GrTime * 10;//綠燈時間的百毫秒
+		if (pause == 0) {
+			if (time == 0) {
+				light = light + 1;
+				if (light == 3)
+					light = 0;
+				switch (light) {
+				case 0:
+					Red(LOW);
+					Green(HIGH);
+					time = GrTime * 10;
+				case 1:
+					Green(LOW);
+					Orange(HIGH);
+					time = 50;
+				case 2:
+					Orange(LOW);
+					Red(HIGH);
+					time = RdTime * 10;
+				}
+			}
+			time--;
+			displayTime(time / 10);
 		}
-	}
+		delay(100);
+	} while (!changeMode());
+}
 
+boolean changeMode()
+{
+
+	return true;
 }
